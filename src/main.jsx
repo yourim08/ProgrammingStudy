bf6fbcdaf3ee3e7ddbbb4ada3a757ed8c39ffcd5
yourm08 <s2413@e-mirim.hs.kr>
2026-04-06 14:39:45 +0900

    TodoListApp reset.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 import CounterApp from './CounterApp.jsx'
+import './reset.css';
 import TodoListApp from './TodoListApp.jsx'
 
 createRoot(document.getElementById('root')).render(
